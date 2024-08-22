be3acc1bde22d269bc30dcb2f68df5c03dcee58f
Techcam1 <167629538+Techcam1@users.noreply.github.com>
2024-08-22 02:03:58 -0400

    more customized

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,7 +27,7 @@ export const Navbar = () => {
           onClick={() => setMenuOpen(false)}
         >
           <li>
-            <a href="#about">About</a>
+            <a href="#about">About Me</a>
           </li>
           <li>
             <a href="#experience">Experience</a>
