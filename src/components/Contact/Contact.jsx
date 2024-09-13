a3d749030b5021666d8b7935602db223dbc39b83
Techcam1 <167629538+Techcam1@users.noreply.github.com>
2024-09-12 21:47:17 -0400

    checking changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="cnc.williams1@gmail.com">Gmail</a>
+          <a href="mailto:cnc.williams1@gmail.com">Gmail</a>
         </li>
         <li className={styles.link}>
           <img
