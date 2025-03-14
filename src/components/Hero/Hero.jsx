77b0478d8a87f7c052d6135cbe278bf0ae4ed2c2
CnWilliams1 <166253595+CnWilliams1@users.noreply.github.com>
2025-03-13 22:44:47 -0400

    updated resume pdf

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Cameron</h1>
         <p className={styles.description}>
-        I'm a UX/UI designer with experience in React, Node.js, Figma, and Adobe XD.
+        I'm a front-end developer with experience in React, Node.js, Figma, and Adobe XD.
         </p>
         <a href="mailto:cnc.williams1@gmail.com" className={styles.contactBtn}>
           Contact Me
